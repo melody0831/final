c460c43b94106ade2739ca45db9f7cac955482c1
Melody Jan <melody0831@gmail.com>
2017-04-30 14:32:10 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,19 +3,19 @@ var speed = 3;
 var cnv;
 
 function setup() {
-  cnv = createCanvas(200, 30);
-    cnv.position(250, 300);
+  cnv = createCanvas(300, 30);
+    cnv.position(230, 250);
     cnv.style('z-index', '2');
 }
 
 function draw() {
-  background(230);
+  background(220);
   stroke(0);
   strokeWeight(2);
   noFill();
   ellipse(x, 20, 10, 10);
 
-  if (x > 200) {
+  if (x > 300) {
     speed = -3;
   } else if (x < -5) {
       speed = 3;
